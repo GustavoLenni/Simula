a8f697ffbf91b9ec9f4e61d21d6d3ba85b0142f7
GustavoLenni <gustavogithub1@gmail.com>
2025-08-04 00:03:09 -0300

    Alteração Login

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,8 +1,10 @@
 import Logo from './images/logo.png';
 import LogoNome from './images/logonome.png';
 import Mascote from './images/mascote.png';
+import MascoteBack from './images/mascote-back.png';
+import MascoteWelcome from './images/mascote-welcome.png';
 const images = {
-    Logo,LogoNome,Mascote
+    Logo,LogoNome,Mascote,MascoteBack,MascoteWelcome
 }
 
 export default images;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,66 @@
-// import Images from '../assets/images';
+import Images from '../assets/images';
 import '../css/login.css';
 
 const login = () => {
     return(
         <div className='Container'>
-            
+
+            <div className='button'>
+                {/* <Link to='/cadastro'></Link> */}
+            </div>
+
+            <div className='Conteudo'>
+                
+                {/* mascote */}
+                <div className='mascote'>
+                    <img alt='mascote' src={Images.MascoteBack}></img>
+                </div>
+
+                {/* campos de Login */}
+                <div className='Login'>
+
+                    <div className='texto'>
+                        <p>Entrar</p>
+                    </div>
+
+                    <div className='inputs'>
+                        <div className='input'>
+                            <input placeholder='Email'></input>
+                        </div>
+                        <div className='input'>
+                            <input placeholder='Senha'></input>
+                        </div>
+                    </div>
+
+                    {/* Linha */}
+                    <div className='Linha'></div>
+                    <div className='text linha'><p>ou</p></div>
+                    <div className='Linha'></div>
+
+                    <div className='buttons'>
+                        {/* 
+                        <div className='button-google'>
+                            <Link to='/google'>
+                                // icon do google
+                            </Link>
+                        </div>
+                         */}
+                         {/* 
+                        <div className='button-facebook'>
+                            <Link to='/facebook'>
+                                // icon do facebook
+                            </Link>
+                        </div>
+                         */}
+                    </div>
+
+
+                    <div className='textos'>
+                        <p>Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem </p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
